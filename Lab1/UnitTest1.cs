8d672cbb434a947e08c2c3e908f444f1428235a1
lukich2000-cloud <lukich.2000@bk.ru>
2025-11-28 01:41:13 +0300

    Add files via upload

---
+++ Lab1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests.Lab1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EmptyCourseTest()
+        {
+            CourseManagment course = new CourseManagment();
+            Boolean res = course.IsEmptyCourse();
+            Assert.True(res);
+        }
+        [Fact]
+        public void EmptyTeacherTest()
+        {
+            CourseManagment course = new CourseManagment();
+            Boolean res = course.IsEmptyTeachers();
+            Assert.False(res);
+        }
+        [Fact]
+        public void getFIOTest()
+        {
+            People people = new Student("Mitya", "Luka", "Viktorovich", 19, "Russia", "K3242", 2, "FPIN");
+            String FIO = people.getFIO();
+            String res = "Luka" + " " + "Mitya" + " " + "Viktorovich";
+            Assert.Equal(res, FIO);
+        }
+    }
+}
